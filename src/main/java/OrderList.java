2ec14b2b30059759deaa7a0b464fe2405890a9b9
nikita <nikitachertkov@gmail.com>
2023-11-15 17:24:03 +0500

    Initial commit

---
+++ src/main/java/OrderList.java
@@ -0,0 +1,76 @@
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+
+public class OrderList {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        long expires = System.currentTimeMillis() / 1000+5;
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+//                .uri(URI.create("https://testnet.bitmex.com/api/v1/order"))
+                .uri(URI.create("https://testnet.bitmex.com/api/v1/order?count=10&reverse=false"))
+                .header("api-expires", String.valueOf(expires))
+                .header("api-key", "DI-FVmnjsNvWGcJLEyVxqncH")
+                .header("api-signature", generateSignature("bv3Z35DKSh7No26QZfYGsx75QBwo8KasCpkD2hKDJ5yLmd7v", "GET",
+                        "https://testnet.bitmex.com/api/v1/order?count=10&reverse=false", expires, "") )
+                .build();
+
+        HttpResponse<String> send = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        System.out.println(send.body());
+
+    }
+
+    private static String generateSignature(String secret, String verb, String url, long expires, String data) {
+        String path = url;
+        String query = "";
+        try {
+            java.net.URL parsedURL = new java.net.URL(url);
+            path = parsedURL.getPath();
+            query = parsedURL.getQuery();
+        } catch (java.net.MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        if (query != null && !query.isEmpty()) {
+            path = path + "?" + query;
+        }
+
+        if (data != null && !data.isEmpty()) {
+            data = URLEncoder.encode(data, StandardCharsets.UTF_8);
+        }
+
+        String message = verb + path + expires + data;
+//        System.out.println("Вычисляем HMAC: " + message);
+
+        String signature = "";
+        try {
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secret_key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+            sha256_HMAC.init(secret_key);
+            byte[] hash = sha256_HMAC.doFinal(message.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            signature = hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (java.security.InvalidKeyException e) {
+            e.printStackTrace();
+        }
+
+        return signature;
+    }
+
+}
---
+++ src/main/java/entity/MyOrder.java
@@ -0,0 +1,96 @@
+package entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class MyOrder {
+    @JsonProperty("orderID")
+    private String orderID;
+
+    @JsonProperty("clOrdID")
+    private String clOrdID;
+
+    @JsonProperty("clOrdLinkID")
+    private String clOrdLinkID;
+
+    @JsonProperty("account")
+    private int account;
+
+    @JsonProperty("symbol")
+    private String symbol;
+
+    @JsonProperty("side")
+    private String side;
+
+    @JsonProperty("orderQty")
+    private int orderQty;
+
+    @JsonProperty("price")
+    private double price;
+
+    @JsonProperty("displayQty")
+    private Double displayQty;
+
+    @JsonProperty("stopPx")
+    private Double stopPx;
+
+    @JsonProperty("pegOffsetValue")
+    private Double pegOffsetValue;
+
+    @JsonProperty("pegPriceType")
+    private String pegPriceType;
+
+    @JsonProperty("currency")
+    private String currency;
+
+    @JsonProperty("settlCurrency")
+    private String settlCurrency;
+
+    @JsonProperty("ordType")
+    private String ordType;
+
+    @JsonProperty("timeInForce")
+    private String timeInForce;
+
+    @JsonProperty("execInst")
+    private String execInst;
+
+    @JsonProperty("contingencyType")
+    private String contingencyType;
+
+    @JsonProperty("ordStatus")
+    private String ordStatus;
+
+    @JsonProperty("triggered")
+    private String triggered;
+
+    @JsonProperty("workingIndicator")
+    private boolean workingIndicator;
+
+    @JsonProperty("ordRejReason")
+    private String ordRejReason;
+
+    @JsonProperty("leavesQty")
+    private int leavesQty;
+
+    @JsonProperty("cumQty")
+    private int cumQty;
+
+    @JsonProperty("avgPx")
+    private double avgPx;
+
+    @JsonProperty("text")
+    private String text;
+
+    @JsonProperty("transactTime")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    private Date transactTime;
+
+    @JsonProperty("timestamp")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    private Date timestamp;
+}
